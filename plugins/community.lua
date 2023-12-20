d5354244329b98c15513a23aa1483ab4f431d6ef
Dmitry Pereboev <41340774+mayidar@users.noreply.github.com>
2023-12-20 15:54:53 +0300

    Add language packs to community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,8 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.bash" },
 }
